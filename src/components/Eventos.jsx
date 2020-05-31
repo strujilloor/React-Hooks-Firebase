681574c0945b9f391ab861a92efd648eb70221ff
Stiven Trujillo <strujillo1293@gmail.com>
2020-05-31 04:17:34 -0500

    Hook de estado

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -1,15 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Eventos = () => {
 
+    const [texto, setTexto] = useState('Texto desde estado');
+
     const eventoClick = () => {
         console.log('me diste un click');
+        setTexto('Cambiando el Texto...');
     }
 
     return (
         <Fragment>
             <hr/>
-            <h2>Eventos</h2>
+            <h2>{ texto }</h2>
             <button 
                 onClick={ () => eventoClick() }
             >Click</button>
