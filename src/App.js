1495d88b7eb1e2d9e46c85d54ae2265c9fd4dfe0
Stiven Trujillo <strujillo1293@gmail.com>
2020-05-31 04:43:19 -0500

    Listas y Key

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
+import Listas from './components/Listas';
 
 // Importar componente
-import Parrafo from './components/Parrafo';
-import Variables from './components/Variables';
-import Eventos from './components/Eventos';
-import Contador from './components/Contador';
+// import Parrafo from './components/Parrafo';
+// import Variables from './components/Variables';
+// import Eventos from './components/Eventos';
+// import Contador from './components/Contador';
 
 function App() {
   return (
     <div className="App">
       <h1>Hola mundo React</h1>
-      <Parrafo />
-
+      {/* <Parrafo />
       <Variables />
-
       <Eventos />
-
-      <Contador />
+      <Contador /> */}
+      <Listas />
     </div>
   );
 }
