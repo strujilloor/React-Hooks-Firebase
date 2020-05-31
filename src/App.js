68011796010cfa0d4267136ac599aa7666ef95f4
Stiven Trujillo <strujillo1293@gmail.com>
2020-05-31 03:38:15 -0500

    Mi primer Componente

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+// Importar componente
+import Parrafo from './components/Parrafo';
+
 function App() {
   return (
     <div className="App">
       <h1>Hola mundo React</h1>
+      <Parrafo />
+      <Parrafo />
+      <Parrafo />
     </div>
   );
 }
---
+++ src/components/Parrafo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Parrafo() {
+    return (
+            <p>Bienvenidos a mi sitio Web.</p>
+    );
+};
+
+export default Parrafo;
