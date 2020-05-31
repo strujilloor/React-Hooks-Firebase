0982fe525f65e845151996ccf68a22a70a851cd1
Stiven Trujillo <strujillo1293@gmail.com>
2020-05-31 03:48:47 -0500

    Introduccion a JSX

---
+++ src/components/Variables.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Variables = () => {
+    const saludo = "Hola desde constante";
+    const imagen = "https://www.promiau.com/static/img/img_blog/cuidados-basicos-para-gatitos-huerfanos.jpg";
+    
+    return (
+        <div>
+            <h2>Variables { saludo }</h2>
+            <img src={ imagen } alt="" width="200" />
+        </div>
+    );
+};
+
+export default Variables;
